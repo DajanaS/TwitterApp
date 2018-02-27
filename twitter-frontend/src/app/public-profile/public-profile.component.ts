66180a7104868cbdee9a047bea004f12fa8fbf20
Dimitar Kovachevikj <dime15mk@gmail.com>
2018-02-27 19:57:18 +0100

    Add tweet likes to public profile component

--- twitter-frontend/src/app/public-profile/public-profile.component.ts
+++ twitter-frontend/src/app/public-profile/public-profile.component.ts
@@ -6,6 +6,8 @@ import {UserManagementService} from '../user-management.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {TweetManagementService} from '../tweet-management.service';
 import {el} from '@angular/platform-browser/testing/src/browser_util';
+import {LikeManagementService} from '../like-management.service';
+import {TweetLike} from '../model/tweetLike';
 
 @Component({
   selector: 'app-public-profile',
@@ -18,17 +20,25 @@ export class PublicProfileComponent implements OnInit {
   currentUser: User;
   authenticatedUser: User;
   currentRate: number;
-  liked = false;
+  currentLiked: boolean[];
+  like: TweetLike;
 
   @Output() totalRate: string;
 
   constructor(private route: ActivatedRoute,
               private modalService: NgbModal,
               private userService: UserManagementService,
-              private tweetService: TweetManagementService) {
+              private tweetService: TweetManagementService,
+              private likeService: LikeManagementService) {
     userService.rateUser$.subscribe(user => {
       this.currentUser = user;
       this.setRating();
+    });
+    likeService.newLikeAdded$.subscribe(like => {
+      this.setTweets();
+    });
+    likeService.likeRemoved$.subscribe(tweetId => {
+      this.setTweets();
     });
   }
 
@@ -42,7 +52,7 @@ export class PublicProfileComponent implements OnInit {
         this.currentUser = user;
         this.setInitialCurrentRate();
         this.setRating();
-        this.tweetService.getTweetsByAuthor(this.currentUser.id).subscribe(tweets => this.tweets = tweets);
+        this.setTweets();
       });
     });
   }
@@ -71,9 +81,52 @@ export class PublicProfileComponent implements OnInit {
     }
   }
 
+  setTweets() {
+    this.tweetService.getTweetsByAuthor(this.currentUser.id).subscribe(tweets => {
+      this.tweets = tweets;
+      let index = 0;
+      this.currentLiked = [];
+      this.currentLiked.length = this.tweets.length;
+      for (const tweet of this.tweets) {
+        const tweetLikes = tweet.likes;
+        tweet.sumLikes = tweetLikes.length;
+        if (tweetLikes.length === 0) {
+          this.currentLiked[index] = false;
+        } else {
+          for (const like of tweetLikes) {
+            if (like.likeOwnerId === this.authenticatedUser.id) {
+              this.currentLiked[index] = true;
+              break;
+            } else {
+              this.currentLiked[index] = false;
+            }
+          }
+        }
+        index++;
+      }
+    });
+  }
+
   rateUser(event) {
     this.userService.rateUser(this.currentUser.id, this.authenticatedUser.id, event).subscribe(user => {
       this.userService.userRated(user);
     });
+  }
+
+  likeTweet(tweetIndex: number) {
+    if (!this.currentLiked[tweetIndex]) {
+      const likedTweetId = this.tweets[tweetIndex].id;
+      this.currentLiked[tweetIndex] = true;
+      this.likeService.addLike(likedTweetId, this.authenticatedUser.id).subscribe(like => {
+        this.like = like;
+        this.likeService.newLikedAdded(this.like);
+      });
+    } else {
+      this.likeService.removeLike(this.tweets[tweetIndex].id, this.authenticatedUser.id).subscribe(resp => {
+        if (resp) {
+          this.likeService.likeRemoved(this.tweets[tweetIndex].id);
+        }
+      });
+    }
   }
 }
